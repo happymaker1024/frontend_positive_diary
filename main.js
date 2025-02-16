c6daabeca10c22174a573b2f927fac8627db51aa
happymaker <happymaker1024@gmail.com>
2025-02-16 18:14:26 +0900

    null 예외 처리, 기본 입력, 저장 코드 완료

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function saveHandler() {
     let text = $("#text").val();
     let mood = $("#mood").val();
 
-    diary = `<p>${date}: ${text} - ${mood}</p>`;
+    diary = `<p>${date}: ${text} - ${mood}</p>` + diary;
 
     $("#result").html(diary);
 
@@ -15,6 +15,12 @@ function saveHandler() {
 $(document).ready(function() {
     // "diary" 키의 값을 읽어옴
     diary = localStorage.getItem("diary");
+
+    // 초기 null 예외처리
+    if (diary == null) {
+        diary = "";
+    }
+
     // html문서에서 id가 result인 요소에 diary 내용 표시
     $("#result").html(diary)
     // html문서에서 id가 save인 요소를 클릭하면 saveHandler 함수 호출
