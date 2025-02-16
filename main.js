6413e5e0eaa40b9af1dfbbe3d0ce59194a66dc4f
happymaker <happymaker1024@gmail.com>
2025-02-16 17:35:15 +0900

    긍정확언 기록장 기본 틀 만들기, html, css, js

---
+++ main.js
@@ -0,0 +1,6 @@
+localStorage.setItem("test-key", "테스트1입니다");
+localStorage.setItem("test-key2", "테스트2입니다");
+let storage1 = localStorage.getItem("test-key");
+let storage2 = localStorage.getItem("test-key2");
+
+console.log(storage1)
